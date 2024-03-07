1a1ad57f9aa7ba9a311d363e63443ed25e8309ed
Lagat100 <weldonkiprop1@gmail.com>
2024-03-07 23:21:18 +0300

    extract heap

---
+++ 133-heap_extract.c
@@ -0,0 +1,123 @@
+#include "binary_trees.h"
+
+/**
+ *  * binary_tree_height - Measures the height of a binary tree
+ *   * @tree: Pointer to the root node of the tree
+ *    *
+ *     * Return: Height of the tree
+ *      */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
+ *  * get_last_node - Gets the last level-order node of a binary tree
+ *   * @root: Pointer to the root node of the tree
+ *    * @height: Height of the tree
+ *     *
+ *      * Return: Pointer to the last level-order node
+ *       */
+heap_t *get_last_node(heap_t *root, size_t height)
+{
+	size_t i;
+	heap_t *last_node = root;
+
+	for (i = 0; i < height - 1; i++)
+	{
+		if (last_node->left)
+			last_node = last_node->left;
+		else
+			last_node = last_node->right;
+	}
+
+	return (last_node);
+}
+
+/**
+ *  * heapify_down - Reorders the heap by moving the given node down to its correct position
+ *   * @root: Pointer to the root node of the heap
+ *    *
+ *     * Return: Pointer to the root node of the heap after heapify-down operation
+ *      */
+heap_t *heapify_down(heap_t *root)
+{
+	heap_t *largest = root;
+	heap_t *left = root->left;
+	heap_t *right = root->right;
+	int temp;
+
+	/* Find the largest among root, left, and right */
+	if (left && left->n > largest->n)
+		largest = left;
+	if (right && right->n > largest->n)
+		largest = right;
+
+	/* If largest is not root, swap root with largest */
+	if (largest != root)
+	{
+		temp = root->n;
+		root->n = largest->n;
+		largest->n = temp;
+		return heapify_down(largest);
+	}
+
+	return root;
+}
+
+/**
+ *  * heap_extract - Extracts the root node of a Max Binary Heap
+ *   * @root: Double pointer to the root node of the heap
+ *    *
+ *     * Return: Value stored in the root node, or 0 on failure
+ *      */
+int heap_extract(heap_t **root)
+{
+	int value;
+	size_t height;
+	heap_t *last_node, *parent;
+
+	if (root == NULL || *root == NULL)
+		return (0);
+
+	/* Store the value of the root node */
+	value = (*root)->n;
+
+	/* Get the height of the heap */
+	height = binary_tree_height(*root);
+
+	/* Get the last node of the heap */
+	last_node = get_last_node(*root, height);
+
+	/* Replace the root node with the last node */
+	if (*root == last_node)
+	{
+		free(*root);
+		*root = NULL;
+	}
+	else
+	{
+		parent = last_node->parent;
+		if (parent->left == last_node)
+			parent->left = NULL;
+		else
+			parent->right = NULL;
+
+		(*root)->n = last_node->n;
+		free(last_node);
+
+		/* Reorder the heap */
+		*root = heapify_down(*root);
+	}
+
+	return (value);
+}
+
